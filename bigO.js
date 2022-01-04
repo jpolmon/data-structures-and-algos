c63586afceb04b81ce6815a67b53a60148010d76
Jonathan Olmon <jonathan.olmon@gmail.com>
2022-01-03 18:24:15 -0600

    Completed the bog O portion of the course.

--- bigO.js
+++ bigO.js
@@ -1,4 +1,18 @@
+const { performance } = require("perf_hooks");
 const nemo = ["nemo"];
+const everyone = [
+  "dory",
+  "bruce",
+  "marlin",
+  "nemo",
+  "gill",
+  "bloat",
+  "nigel",
+  "squirt",
+  "darla",
+  "hank",
+];
+const large = new Array(100).fill("nemo");
 
 function findNemo(array) {
   for (let i = 0; i < array.length; i++) {
@@ -8,4 +22,22 @@ function findNemo(array) {
   }
 }
 
-findNemo(nemo);
+findNemo(large);
+
+function boo(n) {
+  for (let i = 0; i < n.length; i++) {
+    console.log("booooo!");
+  }
+}
+
+boo(["1", "2", "3", "4", "5"]);
+
+function arrayOfHiNTimes(n) {
+  let hiArray = [];
+  for (let i = 0; i < n; i++) {
+    hiArray[i] = "hi";
+  }
+  console.log(hiArray);
+}
+
+arrayOfHiNTimes(6);
