f3c7ebe92405ff87626a16383194f31ef3772832
Jonathan Olmon <jonathan.olmon@gmail.com>
2022-01-18 15:14:11 -0600

    completed the sorting section.

--- sorting.js
+++ sorting.js
@@ -20,7 +20,7 @@ function bubbleSort(array) {
 function selectionSort(array) {
   for (let i = 0; i < array.length; i++) {
     let selection = array[i];
-    for (let j = i; j < array.length; j++) {
+    for (let j = i + 1; j < array.length; j++) {
       array[j] < selection ? (selection = array[j]) : (selection = selection);
     }
     array.splice(array.indexOf(selection), 1);
@@ -56,18 +56,77 @@ function mergeSort(array) {
   // Split array into right and left
   let left = array.slice(0, array.length / 2);
   let right = array.slice(array.length / 2);
-  console.log(left);
-  console.log(right);
   return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
-  for (let num1 of left) {
-    for (let num2 of right) {
+  if (!left) {
+    return right;
+  }
+  if (!right) {
+    return left;
+  }
+  let array = [...left, ...right];
+  let sort = true;
+  let count = 0;
+  while (sort) {
+    let num1 = array[count];
+    let num2 = array[count + 1];
+    num1 > num2
+      ? ((array[count] = num2), (array[count + 1] = num1), (count = 0))
+      : count++;
+    if (count === array.length) {
+      sort = false;
     }
   }
+  return array;
 }
 
-let answer = mergeSort(numbers);
+// let answer = mergeSort(numbers);
 
-console.log(answer);
+// console.log(answer);
+
+function quickSort(array) {
+  if (array.length <= 2) {
+    return array;
+  }
+
+  let pivot = array[Math.floor(Math.random() * (array.length - 1))];
+  console.log(pivot);
+  let left = [];
+  let right = [];
+  right.push(pivot);
+  for (let i = 0; i < array.length; i++) {
+    let temp = array[i];
+    if (temp < pivot) {
+      left.push(temp);
+    } else if (temp > pivot) {
+      right.push(temp);
+    }
+  }
+  return join(quickSort(left), quickSort(right));
+}
+
+function join(left, right) {
+  return [...left, ...right];
+}
+
+console.log(quickSort(numbers));
+
+// _______ INTERVIEW QUESTIONS _________
+
+//#1 - Sort 10 schools around your house by distance: insertion sort since there are only a few schools.
+
+//#2 - eBay sorts listings by the current Bid amount: radix or counting since they are all numbers of fixed length.
+
+//#3 - Sport scores on ESPN: quick sort since it's lighter on space complexity.
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data: Merge sort since you are already using external memory, can't afford worst case of quick sort.
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews: insertion since you have an already sorted list that you are only adding 2 reviews to.
+
+//#6 - Temperature Records for the past 50 years in Canada: radix/counting if you know that the temperatures will not have decimal points. otherwise quick sort.
+
+//#7 - Large user name database needs to be sorted. Data is very random: merge sort if memory isn't too expensive or quick if you are not worried about the worst case.
+
+//#8 - You want to teach sorting for the first time: bubble and selection because they are the most simple of the algorithms.
